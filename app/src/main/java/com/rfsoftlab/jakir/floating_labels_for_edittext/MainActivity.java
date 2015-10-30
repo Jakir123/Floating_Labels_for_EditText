b057a8dc1b5cc0b75658753fe31629d5542c5a1a
jakir <jakir587493B>
2015-10-30 11:52:58 +0600

    initial commit

--- app/src/main/java/com/rfsoftlab/jakir/floating_labels_for_edittext/MainActivity.java
+++ app/src/main/java/com/rfsoftlab/jakir/floating_labels_for_edittext/MainActivity.java
@@ -65,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
 
-        Toast.makeText(getApplicationContext(), "Sign Up Successful!", Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), "CSign Up Successful!", Toast.LENGTH_SHORT).show();
     }
 
     private class MyTextWatcher implements TextWatcher {
